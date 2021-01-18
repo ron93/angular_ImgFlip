085f2a3b240201f51a1d8f06a1f54cd548483f4b
ron93 <ronmkare1@gmail.com>
2021-01-18 13:08:09 +0300

    scrapy spider: items.py -- Product class

--- amazon_scraper/amazon_scraper/items.py
+++ amazon_scraper/amazon_scraper/items.py
@@ -6,7 +6,16 @@
 import scrapy
 
 
-class AmazonScraperItem(scrapy.Item):
+class Product(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    brand = scrapy.Field()
+    rating = scrapy.Field()
+    price = scrapy.Field()
+    colour = scrapy.Field()
+    instock = scrapy.Field()
+    reviews = scrapy.Field()
+    description = scrapy.Field()
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
